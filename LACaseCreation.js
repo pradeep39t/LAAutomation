a092358dcb14aa8b6643e4e4e9b7110db3935356
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2018-07-16 02:37:35 +0530

    Update LACaseCreation.js

--- LACaseCreation.js
+++ LACaseCreation.js
@@ -1,5 +1,11 @@
 function myFunction(){
-	document.getElementsByName('USER')[0].value='WI';	
-	document.getElementsByName('PASSWORD')[0].value='WI';
-	document.getElementById('BUTTON_NEXT1').click();
+	if(document.getElementsByTagName('legend')[0].innerHTML=='Login to your Account'){	
+		document.getElementsByName('USER')[0].value='WI';	
+		document.getElementsByName('PASSWORD')[0].value='WI';
+		document.getElementById('BUTTON_NEXT1').click();
+	}
+	if(document.getElementsByClassName('required_field')[0].innerHTML=='Which Benefits/Services Would You Like to Apply For?'){	
+		document.getElementById('requestFoodShare').checked = true;	
+		document.getElementById('BUTTON_NEXT1').click(); 		
+	}
 }myFunction()
