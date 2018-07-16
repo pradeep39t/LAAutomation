1dd477e3bd3f53990d514e1e3edb3839bfb71f59
pradeep39t <pradeep.panigrahy_nist@yahoo.com>
2018-07-17 02:43:40 +0530

    Update LACaseCreation.js

--- LACaseCreation.js
+++ LACaseCreation.js
@@ -1,4 +1,12 @@
 function myFunction(){
+	if(document.getElementsByTagName('legend')[0].innerHTML=='What action are you trying to perform?'){	
+		document.getElementById('radioGroup1_1').checked = true;	
+		document.getElementById('zipCode').value = 70148;	
+		document.getElementById('BUTTON_NEXT1').click();
+	}
+	if(document.getElementsByTagName('a')[6].innerHTML=='Click here to log in!'){	
+		document.getElementsByTagName('a')[6].click();
+	}	
 	if(document.getElementsByTagName('legend')[0].innerHTML=='Login to your Account'){	
 		document.getElementsByName('USER')[0].value='LATest';	
 		document.getElementsByName('PASSWORD')[0].value='LATest';
@@ -8,10 +16,16 @@ function myFunction(){
 		document.getElementById('agreePolicy').checked = true;	
 		document.getElementById('BUTTON_NEXT1').click();
 	}
-	if(document.getElementsByTagName('legend')[0].innerHTML=='Apply For Benefits/Services'){	
+	if(document.getElementsByClassName('applicationListItem')[0].getElementsByTagName('a')[0].innerHTML=='My Applications'){		
+		document.getElementsByClassName('applicationListItem')[0].getElementsByTagName('a')[0].click();
+	}
+	if(document.getElementsByClassName('MyAccountLargeBoxHeader')[0].innerHTML=='Submit an Application for Benefits/Services'){	
+		document.getElementById('BUTTON_APPLYNOW').click();
+	}		
+	if(document.getElementsByTagName('legend')[1].innerHTML=='Apply For Benefits/Services'){	
 		document.getElementById('BUTTON_NEXT1').click();
 	}	
-	if(document.getElementsByTagName('legend')[0].innerHTML=='Which Benefits/Services Would You Like to Apply For?'){	
+	if(document.getElementsByTagName('legend')[0].innerHTML=='<strong class="required_field">*</strong>Which Benefits/Services Would You Like to Apply For?'){	
 		document.getElementById('requestFoodShare').checked = true;	
 		document.getElementById('BUTTON_NEXT1').click(); 		
 	}
@@ -27,14 +41,12 @@ function myFunction(){
 		document.getElementById('label_7_N').checked = true;	
 		document.getElementById('BUTTON_NEXT1').click();
 	}
-	if(document.getElementsByTagName('legend')[0].innerHTML=='Information About You'){
+	if(document.getElementsByTagName('legend')[1].innerHTML=='Information About You'){
 		document.getElementById('firstName').value = 'Joseph';
 		document.getElementById('lastName').value = 'Shaw';
 		document.getElementById('gender_M').checked = true;	
 		document.getElementById('dateOfBirth').value = '07/22/1985';
 		document.getElementById('seasonalFarmWorker').value = 'N';
-		document.getElementById('jobRecentlyEnded').value = 'N';
-		document.getElementById('moneyFromNewSource').value = 'N';
 		document.getElementById('streetNumber_org').value = 'Richmond Circle';
 		document.getElementById('streetName_org').value = '2nd Street';
 		document.getElementById('city_org').value = 'New Orleans';
@@ -44,6 +56,9 @@ function myFunction(){
 		document.getElementById('cellPhone').value = '287';
 		document.getElementById('phone2cellPhone').value = '985';
 		document.getElementById('phone3cellPhone').value = '2897';
+		document.getElementById('understandEnglish_N').checked = true;
+		document.getElementById('writeYourLettersInEnglishOrSpanish').value = 'E';
+		document.getElementById('radioGroup_intprtr_N').checked = true;
 		document.getElementById('BUTTON_NEXT1').click();
 	}	
 	if(document.getElementsByTagName('legend')[0].innerHTML=='Basic Information Summary'){
@@ -56,14 +71,11 @@ function myFunction(){
 		document.getElementById('requestFoodShare').checked = true;	
 		document.getElementById('socialSecurity').value=Math.floor(Math.random() * 899+100);
 		document.getElementById('ssn2socialSecurity').value=Math.floor(Math.random() * 99+10);
-		document.getElementById('ssn3socialSecurity').value=Math.floor(Math.random() * 8999+1000);		
-		document.getElementById('BUTTON_NEXT1').click();	
-		document.getElementById('uSCitizen').value = '1';	
-		document.getElementById('rfgeArrivalDate').value = '01/01/1930';	
-		document.getElementById('state').value = 'LA';	
-		document.getElementById('country').value = 'OT';	
-		document.getElementById('sponsor_N').checked = true;
-		document.getElementById('loopingQuestion_N').checked = true;		
+		document.getElementById('ssn3socialSecurity').value=Math.floor(Math.random() * 8999+1000);			
+		document.getElementById('uSCitizen').value = '1';
+		document.getElementById('hspcInd_Y').checked = true;
+		document.getElementById('blkInd_race').checked = true;
+		document.getElementById('loopingQuestion_N').checked = true;
 		document.getElementById('BUTTON_NEXT1').click();
 	}	
 }myFunction()
